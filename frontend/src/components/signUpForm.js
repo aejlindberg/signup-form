8036a913c60efdd2e24fa99cbc00202589f5a8d0
Jeanette <aejlindberg@gmail.com>
2018-11-01 15:11:26 +0100

    fetch on submit

--- frontend/src/components/signUpForm.js
+++ frontend/src/components/signUpForm.js
@@ -5,48 +5,79 @@ class SignUpForm extends React.Component {
 constructor(props) {
   super(props)
   this.state = {
-    user: [{
       username: "",
       email: "",
       password: ""
-    }
-    ]
   }
 }
 
-handleUser = event => {
-  this.setState({ user: event.target.value })
+handleUsername = event => {
+  this.setState({ username: event.target.value })
+}
+
+handleEmail = event => {
+  this.setState({ email: event.target.value })
+}
+
+handlePassword = event => {
+  this.setState({ password: event.target.value })
+}
+
+handleSubmit = event => {
+  event.preventDefault()   //prevents the default behavior
+
+  fetch("http://localhost:8080/users", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(this.state)
+  })
+
+  .then(response => {
+    if (response.status === 201) {
+      this.setState({
+        username: "",
+        email: "",
+        password: ""
+      })
+    }
+  })
+  .catch(err => {
+    console.log(err, "ERROR")
+  })
 }
 
   render() {
     return (
       <div className="wrapper">
-        <form id="signUpForm" className="signUpForm" method="post" action="http://localhost:8080/users">
+        <form id="signUpForm" className="signUpForm" onSubmit={this.handleSubmit}>
           <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          onChange={this.handleUser}
-          value={this.state.user.username}
-          required
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={this.handleUsername}
+            value={this.state.username}
+            required
           />
           <input
-          type="text"
-          placeholder="Enter your email address"
-          name="email"
-          onChange={this.handleUser}
-          value={this.state.user.email}
-          required
+            type="text"
+            placeholder="Enter your email address"
+            name="email"
+            onChange={this.handleEmail}
+            value={this.state.email}
+            required
           />
           <input
-          type="text"
-          placeholder="Password"
-          name="password"
-          onChange={this.handleUser}
-          value={this.state.user.password}
-          required />
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={this.handlePassword}
+            value={this.state.password}
+            required />
           <input
-          type="submit"
+            type="submit"
           />
         </form>
       </div>
